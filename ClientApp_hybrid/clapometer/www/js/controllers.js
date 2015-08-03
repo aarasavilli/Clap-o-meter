7aab3c2dc9ef920e0cf3805a05e6ec8b0a8270aa
Yazuvendra <yazuvendra@gmail.com>
2015-08-03 15:06:13 +0530

    Style Changes

--- ClientApp_hybrid/clapometer/www/js/controllers.js
+++ ClientApp_hybrid/clapometer/www/js/controllers.js
@@ -1,6 +1,7 @@
 angular.module('clap.controllers',['ionic'])
-	.controller('ClapController',function($scope, $ionicPopup, $timeout){
+	.controller('ClapController',function($scope, $ionicPopup, $timeout,$ionicSlideBoxDelegate){
 		var socket = io.connect('http://localhost:8001');
+		$scope.counter = 60;
 		$scope.sendClap = function(){
   			 socket.emit('event:new:clap',function(data){
 				console.log('data'+data);
@@ -8,9 +9,42 @@ angular.module('clap.controllers',['ionic'])
 
 		}
 		
-		socket.on('showQuestion',function(queNumber,question,ansOptions){
+		$scope.countdown = function() {
+			stopped = $timeout(function() {
+			   console.log($scope.counter);
+			 $scope.counter--;   
+			 if($scope.counter==0)
+			  $scope.counter=60;	
+		  
+				$scope.countdown();   
+			}, 1000);
+		  };
+				
+		
+		
+  var responseText=['yesCount','noCount','dontCareCount','notAnswered'];
+  var questionAnswered=false;
+   $scope.responsenoCount=function(index){	  
+	   console.log(index + responseText[index]);
+	   socket.emit('questionResponse', $scope.queNumber, responseText[index]);	   
+	   $ionicSlideBoxDelegate.slide(1);
+       $scope.countdown();
+	   questionAnswered=true;
+
+   }
+   
+  socket.on('showQuestion',function(queNumber,question,ansOptions){
+			$scope.questionText = question;
+			$scope.queNumber = queNumber;
+			$ionicSlideBoxDelegate.slide(2);
 			console.log('Answer options are:'+ansOptions);
-			var myPopup = $ionicPopup.show({
+			questionAnswered=false;
+			$timeout(function() {		
+			    if(!questionAnswered)
+					$scope.responsenoCount(3);
+			}, 5000);
+			
+			/*var myPopup = $ionicPopup.show({
     template: question,
     title: 'Quiz',
     subTitle: '',
@@ -28,20 +62,23 @@ angular.module('clap.controllers',['ionic'])
         onTap: function(e) { return 'yesCount'; }
       }
     ]
-  });
-  myPopup.then(function(response) {
+  }); */
+ 
+  /*myPopup.then(function(response) {
+	  $ionicSlideBoxDelegate.slide(1);
+
+	  $scope.countdown();
 	if(response)
 		socket.emit('questionResponse', queNumber, response);
 	else
 		socket.emit('questionResponse', queNumber, 'notAnswered');
-  });
+  }); 
   $timeout(function() {
      myPopup.close(); //close the popup after some seconds 
-  }, 10000);
+  }, 5000); */
  
-
-   
 
 		});
 
 	});
+	
